ba90d4b6231e1aac97a454842b34c6e5f0f9b3c4
SofiaMp <58699690+SofiaMp@users.noreply.github.com>
2020-08-21 11:02:44 +0200

    added decorators to run from command line

--- src/Python/metadata_examples.py
+++ src/Python/metadata_examples.py
@@ -1,21 +1,31 @@
+import click
 import glob
 import pandas as pd
-from IPython.display import display
-
-pd.set_option('display.max_columns', 23)
-
-path = "../../vocabulary/*.csv"
-examples = pd.DataFrame()
-
-for file in glob.glob(path):
-    approach = pd.read_csv(file)
-    examples = examples.append(approach, ignore_index=True)
 
 
+def read_data():
+    path = "../../vocabulary/*.csv"
+    examples = pd.DataFrame()
+
+    for file in glob.glob(path):
+        approach = pd.read_csv(file, dtype=object)
+        examples = examples.append(approach, ignore_index=True, sort=False)
+    return examples
+
+
+@click.command()
+@click.argument('field')
 def lookup_metadata(field):
-    line = examples[examples['field_id'] == field]
-    line = line.dropna(how='all', axis=1)
-    return line
+    examples = read_data()
+    row = examples[examples['field_id'] == field]
+    #value_names = row['value_name'].values.tolist()
+    row = row.iloc[0]
+    print(f"The field_ id {row['field_id']}; {row['name']} is mapped to the target concept id {row['target_concept_id']}")
+    print(f"The mapping approach to take is {row['mapping_approach']}")
+    print(f"The date when the data was recorded is given in {row['date_field_id']}")
+    print(f"The unit concept id is {row['unit_concept_id']} and the conversion factor is {row['conversion_factor']}")
+    #print(f"The values it can take are {value_names}")
 
 
-display(lookup_metadata(2986))
+if __name__ == "__main__":
+    lookup_metadata()
--- src/Python/omop_table_examples.py
+++ src/Python/omop_table_examples.py
@@ -1,11 +1,22 @@
 import pandas as pd
 import requests
 import json
+import click
+import glob
 
-from src.Python.metadata_examples import examples
+
+def read_data():
+    path = "../../vocabulary/*.csv"
+    examples = pd.DataFrame()
+
+    for file in glob.glob(path):
+        approach = pd.read_csv(file, dtype=object)
+        examples = examples.append(approach, ignore_index=True, sort=False)
+    return examples
 
 
 def omop_domain(field):
+    examples = read_data()
     concept_id = int(examples[examples['field_id'] == field]['target_concept_id'].unique())
     response = requests.get(f'https://athena.ohdsi.org/api/v1/concepts/{concept_id}')
     concept = json.loads(response.text)
@@ -13,22 +24,38 @@ def omop_domain(field):
     return domain
 
 
-def example_omop_table(field, instance, value=None):
-    domain = omop_domain(field)
-    if value is not None:
-        row = examples[(examples['field_id'] == field) & (examples['value'] == value)]
-    else:
-        row = examples[examples['field_id'] == field]
+def create_column_name(field_id, instance_id, array_index):
+    return f'{field_id}-{instance_id}.{array_index}'
 
+
+@click.command()
+@click.argument('field_id')
+@click.option('--value', default=None)
+@click.option('--instance', default='0')
+@click.option('--array', default='0')
+def example_omop_table(field_id, value, instance, array):
+    examples = read_data()
+    domain = omop_domain(field_id).lower()
+    if value is not None:
+        row = examples[(examples['field_id'] == field_id) & (examples['value'] == value)]
+    else:
+        row = examples[examples['field_id'] == field_id]
+    row = row.iloc[0]
     table = pd.Series({'person_id': 'get(eid)',
-                       domain + '_concept_id': int(row.iloc[0]['target_concept_id']),
-                       domain + '_date': 'get date from field ' + str(row.iloc[0]['date_field_id']) + '-' + instance + '.0',
-                       'value_as_concept_id': row.iloc[0]['target_value_concept_id'],
-                       'value_as_number': 'get value from field ' + str(row.iloc[0]['field_id']) + '-' + instance + '.0',
-                       'unit_concept_id': row.iloc[0]['unit_concept_id']
+                       domain + '_date': 'get(%s)' % create_column_name(row['date_field_id'], instance, array),
+                       domain + '_concept_id': row['target_concept_id'],
+                       domain + '_source_value': field_id,
+                       'value_as_concept_id': row["target_value_concept_id"],
+                       'value_as_number': 'get(%s)' % create_column_name(field_id, instance, array),
+                       'value_source_value': value,
+                       'unit_concept_id': row['unit_concept_id'],
+                       domain + '_type_concept_id': 'TBD'
                        })
     table = table.dropna()
-    return table
+    print(domain.upper() + ' table')
+    print(table)
 
 
-print(example_omop_table(2986, '1', 0))
+if __name__ == '__main__':
+    # Note: the arguments have to be strings, not integers.
+    example_omop_table()
